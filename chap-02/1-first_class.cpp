030e645ea7fd2e7d1dc43449ca8ee85e28713b50
Kévin Benoit <kev.br77@gmail.com>
2025-01-19 22:43:17 +0100

    # end of ex1

--- chap-02/1-first_class.cpp
+++ chap-02/1-first_class.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 
+class Person
+{
+    public:
+        void set_name(std::string name){
+            _name = name;
+        }
+
+        void set_age(unsigned int age){
+            _age =age;
+        }
+
+        std::string get_name() const{
+            return _name;
+        }
+
+        unsigned int get_age() const{
+            return _age;
+        }
+
+    private:
+        std::string _name;
+        unsigned int _age;
+};
+
 int main()
 {
-    // Person p;
+    Person p = {};
 
-    // p.set_name("Batman");
-    // p.set_age(23);
+    p.set_name("Batman");
+    p.set_age(23);
 
-    // std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
+    std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
 
     return 0;
 }
